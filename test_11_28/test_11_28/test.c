3526dbc22af53ab836bc3716f5bd8c0188ae38db
xylx521 <hx384391724@163.com>
2023-11-28 23:56:37 +0800

    第三次作业代码

--- test_11_28/test_11_28/test.c
+++ test_11_28/test_11_28/test.c
@@ -18,28 +18,49 @@
 
 //问题：对scanf的返回值遗忘
 
+//#include <stdio.h>
+//
+//int main()
+//{
+//    long long a = 0;
+//    long long b = 0;
+//    while (scanf("%lld %lld", &a, &b) == 2)
+//    {
+//        if (a > b)
+//        {
+//            printf("%lld>%lld\n", a, b);
+//        }
+//        else if (a == b)
+//        {
+//            printf("%lld=%lld\n", a, b);
+//        }
+//        else
+//        {
+//            printf("%lld<%lld\n", a, b);
+//        }
+//    }
+//    return 0;
+//}
+
+//问题：对数据类型长度遗忘，还有printf函数占位符的使用。以及\n换行符忘记使用。
+
+
 #include <stdio.h>
 
 int main()
 {
-    long long a = 0;
-    long long b = 0;
-    while (scanf("%lld %lld", &a, &b) == 2)
+    unsigned int num = 0;
+    scanf("%d", &num);
+    int a = num % 5;
+    if (a == 0)
     {
-        if (a > b)
-        {
-            printf("%lld>%lld\n", a, b);
-        }
-        else if (a == b)
-        {
-            printf("%lld=%lld\n", a, b);
-        }
-        else
-        {
-            printf("%lld<%lld\n", a, b);
-        }
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
     }
     return 0;
 }
 
-//问题：对数据类型长度遗忘，还有printf函数占位符的使用。以及\n换行符忘记使用。
+//问题：语句末尾忘记加分号；
